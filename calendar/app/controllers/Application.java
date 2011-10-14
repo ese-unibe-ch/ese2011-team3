79f8bea48220e84f69906763816c6cc3825f01b7
Dominik Feller <dominik.feller@students.unibe.ch>
2011-10-14 18:05:00 +0200

    added showCalendars and changed Index.html
    fixes #15

--- calendar/app/controllers/Application.java
+++ calendar/app/controllers/Application.java
@@ -1,17 +1,22 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import java.util.List;
 
-import java.util.*;
-
-import models.*;
+import models.User;
+import play.mvc.Controller;
+import play.mvc.With;
 
 @With(Secure.class)
 public class Application extends Controller {
 
-    public static void index() {
-        render();
-    }
+	public static void index() {
+		render();
+	}
+
+	public static void showCalendars(String nickname) {
+		User user = User.find("byNickname", nickname).first();
+		List calendars = user.calendars;
+		render(user, calendars);
+	}
 
 }
