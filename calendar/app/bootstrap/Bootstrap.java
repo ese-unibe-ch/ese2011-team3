df45cd971d8416d71a2c6a42b57636dfb8b72143
Alexander <a.rueedlinger@gmail.com>
2011-10-14 18:10:24 +0200

    added calendars to bootstrap 	modified:   calendar/app/bootstrap/Bootstrap.java

--- calendar/app/bootstrap/Bootstrap.java
+++ calendar/app/bootstrap/Bootstrap.java
@@ -1,5 +1,6 @@
 package bootstrap;
 
+import models.Calendar;
 import models.User;
 import play.jobs.Job;
 import play.jobs.OnApplicationStart;
@@ -10,13 +11,27 @@ public class Bootstrap extends Job {
 	@Override
 	public void doJob() {
 		if (User.count() == 0) {
-			new User("wuschu", "Domi Jason", "secret", "domi@gmail.com").save();
-			new User("lexy", "lex McColin", "secret", "lex@gmail.com").save();
-			new User("king_julien", "Julien Peterson", "secret",
+			User user1 = new User("wuschu", "Domi Jason", "secret",
+					"domi@gmail.com").save();
+			User user2 = new User("lexy", "lex McColin", "secret",
+					"lex@gmail.com").save();
+			User user3 = new User("king_julien", "Julien Peterson", "secret",
 					"king@gmail.com").save();
-			new User("joe", "Joe Gargamel", "secret", "gargamel@gmail.com")
-					.save();
+			User user4 = new User("joe", "Joe Gargamel", "secret",
+					"gargamel@gmail.com").save();
+
+			new Calendar("Home", user1).save();
+			new Calendar("Work", user1).save();
+
+			new Calendar("Home", user2).save();
+			new Calendar("Work", user2).save();
+
+			new Calendar("Sports", user3).save();
+			new Calendar("Uni", user3).save();
+
+			new Calendar("My Calendar", user4).save();
+			new Calendar("My Second Calendar", user4).save();
+
 		}
 	}
-
 }
