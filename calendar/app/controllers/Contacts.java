dc3d5c56fafeb81b5b3f75a9f1adce2474b4a4bf
Julian Pollak <julian.pollak@students.unibe.ch>
2011-10-15 20:13:23 +0200

    Fixed a bug from @lexruee last commit. fixes #24

--- calendar/app/controllers/Contacts.java
+++ calendar/app/controllers/Contacts.java
@@ -1,7 +1,5 @@
 package controllers;
 
-
-
 import java.util.List;
 
 import models.User;
@@ -10,29 +8,29 @@ import play.mvc.With;
 
 @With(Secure.class)
 public class Contacts extends Controller {
-	
-	public static void index(){
-		User user = User.find("byNickname", Security.connected());
+
+	public static void index() {
+		User user = User.find("byNickname", Security.connected()).first();
 		List<User> contacts = user.following;
 		render(contacts);
 	}
-	
-	public static void addContact(Long contactId){
-		User user = User.find("byNickname", Security.connected());
+
+	public static void addContact(Long contactId) {
+		User user = User.find("byNickname", Security.connected()).first();
 		User contact = User.findById(contactId);
 		user.following.add(contact);
 		index();
 	}
-	
-	public static void addContacts(){
-		User user = User.find("byNickname", Security.connected());
+
+	public static void addContacts() {
+		User user = User.find("byNickname", Security.connected()).first();
 		List<User> contacts = User.findAll();
 		contacts.removeAll(user.following);
 		render(contacts);
 	}
-	
-	public static void removeContact(Long contactId){
-		User user = User.find("byNickname", Security.connected());
+
+	public static void removeContact(Long contactId) {
+		User user = User.find("byNickname", Security.connected()).first();
 		User contact = User.findById(contactId);
 		user.following.remove(contact);
 		index();
