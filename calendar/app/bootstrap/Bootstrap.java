38ceb92bacb536f0e8d47c403642b67e6b937e93
Alexander <a.rueedlinger@gmail.com>
2011-10-15 02:41:00 +0200

    made a .yml file for the bootstrap fixes #21 	modified:   calendar/app/bootstrap/Bootstrap.java 	new file:   calendar/conf/bootstrap-data.yml

--- calendar/app/bootstrap/Bootstrap.java
+++ calendar/app/bootstrap/Bootstrap.java
@@ -1,9 +1,9 @@
 package bootstrap;
 
-import models.Calendar;
 import models.User;
 import play.jobs.Job;
 import play.jobs.OnApplicationStart;
+import play.test.Fixtures;
 
 @OnApplicationStart
 public class Bootstrap extends Job {
@@ -11,27 +11,7 @@ public class Bootstrap extends Job {
 	@Override
 	public void doJob() {
 		if (User.count() == 0) {
-			User user1 = new User("wuschu", "Domi Jason", "secret",
-					"domi@gmail.com").save();
-			User user2 = new User("lexy", "lex McColin", "secret",
-					"lex@gmail.com").save();
-			User user3 = new User("king_julien", "Julien Peterson", "secret",
-					"king@gmail.com").save();
-			User user4 = new User("joe", "Joe Gargamel", "secret",
-					"gargamel@gmail.com").save();
-
-			new Calendar("Home", user1).save();
-			new Calendar("Work", user1).save();
-
-			new Calendar("Home", user2).save();
-			new Calendar("Work", user2).save();
-
-			new Calendar("Sports", user3).save();
-			new Calendar("Uni", user3).save();
-
-			new Calendar("My Calendar", user4).save();
-			new Calendar("My Second Calendar", user4).save();
-
+			Fixtures.loadModels("bootstrap-data.yml");
 		}
 	}
 }
