96224e3fa565dcc8b95c85cb02be375e98a65fdd
Alexander <a.rueedlinger@gmail.com>
2011-10-15 17:08:21 +0200

    added events to bootstrap and added two tags called eventsTag and calendarTag. besides that i created a new view showCalendar.html 	modified:   calendar/app/controllers/Application.java 	new file:   calendar/app/views/Application/showCalendar.html 	modified:   calendar/app/views/Application/showCalendars.html 	new file:   calendar/app/views/tags/calendarTag.html 	new file:   calendar/app/views/tags/eventsTag.html 	modified:   calendar/conf/bootstrap-data.yml 	modified:   calendar/conf/routes 	new file:   calendar/public/stylesheets/calendar.css

--- calendar/app/controllers/Application.java
+++ calendar/app/controllers/Application.java
@@ -2,6 +2,8 @@ package controllers;
 
 import java.util.List;
 
+import models.Calendar;
+import models.Event;
 import models.User;
 import play.mvc.Before;
 import play.mvc.Controller;
@@ -30,6 +32,13 @@ public class Application extends Controller {
 		User user = User.find("byNickname", nickname).first();
 		List calendars = user.calendars;
 		render(user, calendars);
+	}
+	
+	public static void showCalendar(String nickname, long id){
+		User user = User.find("byNickname", nickname).first();
+		Calendar calendar = Calendar.findById(id);
+		List<Event> events =calendar.events;
+		render(user, calendar, events);
 	}
 
 }
