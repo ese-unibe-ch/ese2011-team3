cfa6c9499396267779d41392e43accc3fc5bf17f
Alexander <a.rueedlinger@gmail.com>
2011-10-15 20:09:02 +0200

    Created a Contacts Controller and adjusted the routes conf file. fixes 21 fixes 28 	new file:   calendar/app/controllers/Contacts.java 	modified:   calendar/conf/routes

---
+++ calendar/app/controllers/Contacts.java
@@ -0,0 +1,40 @@
+package controllers;
+
+
+
+import java.util.List;
+
+import models.User;
+import play.mvc.Controller;
+import play.mvc.With;
+
+@With(Secure.class)
+public class Contacts extends Controller {
+	
+	public static void index(){
+		User user = User.find("byNickname", Security.connected());
+		List<User> contacts = user.following;
+		render(contacts);
+	}
+	
+	public static void addContact(Long contactId){
+		User user = User.find("byNickname", Security.connected());
+		User contact = User.findById(contactId);
+		user.following.add(contact);
+		index();
+	}
+	
+	public static void addContacts(){
+		User user = User.find("byNickname", Security.connected());
+		List<User> contacts = User.findAll();
+		contacts.removeAll(user.following);
+		render(contacts);
+	}
+	
+	public static void removeContact(Long contactId){
+		User user = User.find("byNickname", Security.connected());
+		User contact = User.findById(contactId);
+		user.following.remove(contact);
+		index();
+	}
+}
