b5a73185d96a516b34006ae367684cdfe5e49267
Alexander <a.rueedlinger@gmail.com>
2011-10-15 11:54:45 +0200

    Created a tag called logoutTag (for a logout link) and modified the main.html file. The Application class has also been modified in order to provide the currently logged in user for the tag view. fixes #23 	modified:   calendar/app/controllers/Application.java 	modified:   calendar/app/views/main.html 	new file:   calendar/app/views/tags/logoutTag.html

--- calendar/app/controllers/Application.java
+++ calendar/app/controllers/Application.java
@@ -3,12 +3,25 @@ package controllers;
 import java.util.List;
 
 import models.User;
+import play.mvc.Before;
 import play.mvc.Controller;
 import play.mvc.With;
 
 @With(Secure.class)
 public class Application extends Controller {
-
+	
+	/**
+	 * this method creates a user object of the currently logged in user and
+	 * makes it accessible for all views and tag views.
+	 */
+	@Before
+	static void setConnectedUser() {
+		if (Security.isConnected()) {
+			User account = User.find("byNickname", Security.connected()).first();
+			renderArgs.put("account", account);
+		}
+	}
+	   
 	public static void index() {
 		Application.showCalendars(Security.connected());
 	}
