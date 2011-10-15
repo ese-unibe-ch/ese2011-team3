1f39601a194c6e446382382bb43b6f89f03654e9
Alexander <a.rueedlinger@gmail.com>
2011-10-15 19:12:21 +0200

    Squashed commit of the following:
    commit 1ac5183c6f8c662de5e0c53805d236e87c0b6f4d
    Merge: 17e733a a23b251
    Author: Alexander <a.rueedlinger@gmail.com>
    Date:   Sat Oct 15 19:11:13 2011 +0200
    
        Merge remote-tracking branch 'origin/master' into alex
    
        Conflicts:
        	calendar/app/controllers/Application.java
        	calendar/conf/routes
    
    commit 17e733ae3b81cec928bc615cd5c79fcda1a5efcb
    Author: Alexander <a.rueedlinger@gmail.com>
    Date:   Sat Oct 15 19:03:35 2011 +0200
    
        worked on the calendar view and added a new dependency (jodatime) to dependencies.yml
        	modified:   calendar/app/controllers/Application.java
        	modified:   calendar/app/views/Application/showCalendar.html
        	modified:   calendar/app/views/tags/calendarTag.html
        	modified:   calendar/conf/dependencies.yml
    
    commit a23b251ab9df941bcbd597a2c1710b1d016196ae
    Author: Thomas Schwander <tom_schwander@hotmail.com>
    Date:   Sat Oct 15 18:02:37 2011 +0200
    
        Added the view for all other users. Added the compareTo method to the User class to sort the users by nickname. Added a link on the showCalendars page.
        fixes #16
    
    commit bb9b1db20b65a6f331fdaab34393b6246c39430d
    Merge: 96224e3 e3d74bf
    Author: Alexander <a.rueedlinger@gmail.com>
    Date:   Sat Oct 15 17:09:18 2011 +0200
    
        Merge remote-tracking branch 'origin/master' into alex
    
    commit e3d74bf9fde2db5aa2b52d3fecdaef8df4260128
    Author: Julian Pollak <julian.pollak@students.unibe.ch>
    Date:   Sat Oct 15 14:29:10 2011 +0200
    
        Removed the D:\play\modules\secure import play did on windows.
        This is not needed at all.
    
        fixes #22.

--- calendar/app/controllers/Application.java
+++ calendar/app/controllers/Application.java
@@ -1,6 +1,8 @@
 package controllers;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 
 import models.Calendar;
 import models.Event;
@@ -38,7 +40,19 @@ public class Application extends Controller {
 		User user = User.find("byNickname", nickname).first();
 		Calendar calendar = Calendar.findById(id);
 		List<Event> events =calendar.events;
-		render(user, calendar, events);
+		
+		Date aDate = new Date();
+		Locale aLocale = new Locale("en", "CH");
+		render(user, calendar, events, aDate, aLocale);
 	}
 
+	public static void showOtherUsers(String userNickname) {
+		User user = User.find("byNickname", userNickname).first();
+		List users = User.findAll();
+		for (int i=0;i<users.size();i++)
+			if (user.equals(users.get(i)))
+				users.remove(i);
+		Collections.sort(users);
+		render(users);
+	}
 }
