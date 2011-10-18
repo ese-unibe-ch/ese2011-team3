44eddb3ea95ebe9b4a6bdf601a97902513aa5626
wuschu <wuschu@ubuntu.(none)>
2011-10-18 17:02:30 +0200

    added a variable isPublic in the Event.java model. corresponding to this i changed the constructor. updated where i needed to be updated. e.g. in the EventTest. made another field in the addEvent.html, the value is not transmitted yet.

--- calendar/app/controllers/Events.java
+++ calendar/app/controllers/Events.java
@@ -19,13 +19,15 @@ public class Events extends Controller {
 
 	public static void createEvent(Long calendarId, @Required String name,
 			@Required Date startDate, @Required Date endDate,
-			@Required String startTime, @Required String endTime, String note) {
-
+			@Required String startTime, @Required String endTime,
+			@Required boolean isPublic, String note) {
+		System.out.println(isPublic);
 		validation.required(name);
 		validation.required(startDate);
 		validation.required(endDate);
 		validation.required(startTime);
 		validation.required(endTime);
+		validation.required(isPublic);
 
 		if (validation.hasErrors()) {
 			params.flash();
@@ -36,7 +38,7 @@ public class Events extends Controller {
 		Calendar calendar = Calendar.findById(calendarId);
 
 		Event event = helperBuildEvent(new Event(name, startDate, endDate,
-				calendar, note), startTime, endTime);
+				calendar, isPublic, note), startTime, endTime);
 		event.save();
 
 		String nickname = calendar.owner.nickname;
@@ -66,13 +68,15 @@ public class Events extends Controller {
 
 	public static void updateEvent(Long eventId, @Required String name,
 			@Required Date startDate, @Required Date endDate,
-			@Required String startTime, @Required String endTime, String note) {
+			@Required String startTime, @Required String endTime,
+			@Required boolean isPublic, String note) {
 
 		validation.required(name);
 		validation.required(startDate);
 		validation.required(endDate);
 		validation.required(startTime);
 		validation.required(endTime);
+		validation.required(isPublic);
 
 		if (validation.hasErrors()) {
 			params.flash();
