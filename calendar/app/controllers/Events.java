28da05a5b5b5ee1116cdfe9b7d5bd3f544a1568b
Alexander <a.rueedlinger@gmail.com>
2011-10-19 21:35:39 +0200

    removed this line of code in the event controller: //editEvent(eventId); //Why was this here? should not render the edit view, but go back to calendar @Niclas found this, he's right, thus i removed it. this was for testing purposes only, to see if the changes have been saved.
    	modified:   calendar/app/controllers/Events.java

--- calendar/app/controllers/Events.java
+++ calendar/app/controllers/Events.java
@@ -16,104 +16,103 @@ import play.mvc.With;
 
 @With(Secure.class)
 public class Events extends Controller {
-	// HH:mm
-	private static final String regexTime = "^([0-1][0-9]|[2][0-3]):([0-5][0-9])$";
+    // HH:mm
+    private static final String regexTime = "^([0-1][0-9]|[2][0-3]):([0-5][0-9])$";
 
-	public static void createEvent(Long calendarId, @Required String name,
-			@Required Date startDate, @Required Date endDate,
-			@Required String startTime, @Required String endTime,
-			@Required boolean isPublic, String note) {
+    public static void createEvent(Long calendarId, @Required String name,
+	    @Required Date startDate, @Required Date endDate,
+	    @Required String startTime, @Required String endTime,
+	    @Required boolean isPublic, String note) {
 
-		validation.required(name);
-		validation.required(startDate);
-		validation.required(endDate);
-		validation.required(isPublic);
-		validation.match(startTime, regexTime).message("Invalid!");
-		validation.match(endTime, regexTime).message("Invalid!");
+	validation.required(name);
+	validation.required(startDate);
+	validation.required(endDate);
+	validation.required(isPublic);
+	validation.match(startTime, regexTime).message("Invalid!");
+	validation.match(endTime, regexTime).message("Invalid!");
 
-		if (validation.hasErrors()) {
-			params.flash();
-			validation.keep();
-			addEvent(calendarId);
-		}
-
-		Calendar calendar = Calendar.findById(calendarId);
-
-		startDate = helperCreateDate(startDate, startTime, "HH:mm");
-		endDate = helperCreateDate(endDate, endTime, "HH:mm");
-		
-		Event event = new Event(name, startDate, endDate, calendar,
-				isPublic, note);
-
-		event.save();
-
-		String nickname = calendar.owner.nickname;
-		Calendars.showCalendar(nickname, calendarId);
+	if (validation.hasErrors()) {
+	    params.flash();
+	    validation.keep();
+	    addEvent(calendarId);
 	}
 
-	private static Date helperCreateDate(Date date, String timeString,
-			String pattern) {
-		DateTimeFormatter parser = DateTimeFormat.forPattern("HH:mm");
-		DateTime time = parser.parseDateTime(timeString);
-		DateTime aDate = new DateTime(date);
-		aDate = aDate.withTime(time.getHourOfDay(), time.getMinuteOfHour(),
-				time.getSecondOfMinute(), time.getMillisOfSecond());
-		return aDate.toDate();
+	Calendar calendar = Calendar.findById(calendarId);
+
+	startDate = helperCreateDate(startDate, startTime, "HH:mm");
+	endDate = helperCreateDate(endDate, endTime, "HH:mm");
+
+	Event event = new Event(name, startDate, endDate, calendar, isPublic,
+		note);
+
+	event.save();
+
+	String nickname = calendar.owner.nickname;
+	Calendars.showCalendar(nickname, calendarId);
+    }
+
+    private static Date helperCreateDate(Date date, String timeString,
+	    String pattern) {
+	DateTimeFormatter parser = DateTimeFormat.forPattern("HH:mm");
+	DateTime time = parser.parseDateTime(timeString);
+	DateTime aDate = new DateTime(date);
+	aDate = aDate.withTime(time.getHourOfDay(), time.getMinuteOfHour(),
+		time.getSecondOfMinute(), time.getMillisOfSecond());
+	return aDate.toDate();
+    }
+
+    public static void updateEvent(Long eventId, @Required String name,
+	    @Required Date startDate, @Required Date endDate,
+	    @Required String startTime, @Required String endTime,
+	    @Required boolean isPublic, String note) {
+
+	validation.required(name);
+	validation.required(startDate);
+	validation.required(endDate);
+	validation.match(startTime, regexTime).message("Invalid!");
+	validation.match(endTime, regexTime).message("Invalid!");
+	validation.required(isPublic);
+
+	if (validation.hasErrors()) {
+	    params.flash();
+	    validation.keep();
+	    editEvent(eventId);
 	}
 
-	public static void updateEvent(Long eventId, @Required String name,
-			@Required Date startDate, @Required Date endDate,
-			@Required String startTime, @Required String endTime,
-			@Required boolean isPublic, String note) {
+	Event event = Event.findById(eventId);
+	event.name = name;
+	event.start = helperCreateDate(startDate, startTime, "HH:mm");
+	event.end = helperCreateDate(endDate, endTime, "HH:mm");
+	event.lowerBound = Event.makeLowerBound(startDate);
+	event.upperBound = Event.makeUpperBound(endDate);
+	event.note = note;
+	event.isPublic = isPublic;
 
-		validation.required(name);
-		validation.required(startDate);
-		validation.required(endDate);
-		validation.match(startTime, regexTime).message("Invalid!");
-		validation.match(endTime, regexTime).message("Invalid!");
-		validation.required(isPublic);
-		
-		if (validation.hasErrors()) {
-			params.flash();
-			validation.keep();
-			editEvent(eventId);
-		}
+	event.save();
 
-		Event event = Event.findById(eventId);
-		event.name = name;
-		event.start = helperCreateDate(startDate, startTime, "HH:mm");
-		event.end = helperCreateDate(endDate, endTime, "HH:mm");
-		event.lowerBound = Event.makeLowerBound(startDate);
-		event.upperBound = Event.makeUpperBound(endDate);
-		event.note = note;
-		event.isPublic = isPublic;
+	String nickname = event.calendar.owner.nickname;
+	Long calendarId = event.calendar.id;
+	Calendars.showCalendar(nickname, calendarId);
+    }
 
-		event.save();
-		//editEvent(eventId); //Why was this here? should not render the edit view, but go back to calendar
-		
-		String nickname = event.calendar.owner.nickname;
-		Long calendarId = event.calendar.id;
-		Calendars.showCalendar(nickname, calendarId);
-	}
+    public static void editEvent(Long eventId) {
+	Event event = Event.findById(eventId);
+	Calendar calendar = event.calendar;
+	User user = calendar.owner;
+	render(calendar, event, user);
+    }
 
-	public static void editEvent(Long eventId) {
-		Event event = Event.findById(eventId);
-		Calendar calendar = event.calendar;
-		User user = calendar.owner;
-		render(calendar, event, user);
-	}
+    public static void addEvent(Long calendarId) {
+	Calendar calendar = Calendar.findById(calendarId);
+	User user = calendar.owner;
+	render(calendar, user);
+    }
 
-	public static void addEvent(Long calendarId) {
-		Calendar calendar = Calendar.findById(calendarId);
-		User user = calendar.owner;
-		render(calendar, user);
-	}
-
-	public static void removeEvent(Long eventId) {
-		Event event = Event.findById(eventId);
-		event.delete();
-		String nickname = event.calendar.owner.nickname;
-		Long calendarId = event.calendar.id;
-		Calendars.showCalendar(nickname, calendarId);
-	}
+    public static void removeEvent(Long eventId) {
+	Event event = Event.findById(eventId);
+	event.delete();
+	String nickname = event.calendar.owner.nickname;
+	Long calendarId = event.calendar.id;
+	Calendars.showCalendar(nickname, calendarId);
+    }
 }
