4e8fb2015ed00377f32d2bce86acb866c5b76b69
Dominik Feller <dominik.feller@students.unibe.ch>
2011-10-20 13:52:17 +0200

    fix the manipulation button for either private or public meanings.
    fixes #57
    
    Squashed commit of the following:
    
    commit 1e18343a66ae3eee3ad6a4fc2097df104816fd6a
    Author: Dominik Feller <dominik.feller@students.unibe.ch>
    Date:   Thu Oct 20 13:20:20 2011 +0200
    
        the logged user has no longer the possibility to edit or remove any event of another user.
        the links are gone.
    
        modified:   calendar/app/views/Calendars/showCalendar.html
        modified:   calendar/app/views/tags/eventsTag.html
    
    commit 6bf4931e75932c1f73e84b5751e27a00b115f53b
    Author: Dominik Feller <dominik.feller@students.unibe.ch>
    Date:   Thu Oct 20 13:00:36 2011 +0200
    
        change the view of the calendar, so that if the logged in user watches a calendar of another user, he is not able to add an event.
    
        modified:   calendar/app/controllers/Calendars.java
        modified:   calendar/app/views/Calendars/showCalendar.html

--- calendar/app/controllers/Calendars.java
+++ calendar/app/controllers/Calendars.java
@@ -19,53 +19,53 @@ import play.mvc.With;
 @With(Secure.class)
 public class Calendars extends Application {
 
-    public static void showCalendars(String nickname) {
-	User user = User.find("byNickname", nickname).first();
-	List<Calendar> calendars = user.calendars;
-	render(user, calendars);
-    }
+	public static void showCalendars(String nickname) {
+		User user = User.find("byNickname", nickname).first();
+		List<Calendar> calendars = user.calendars;
+		render(user, calendars);
+	}
 
-    public static void showCalendar(String nickname, Long id) {
-	User user = User.find("byNickname", nickname).first();
-	Calendar calendar = Calendar.findById(id);
+	public static void showCalendar(String nickname, Long id) {
+		User user = User.find("byNickname", Security.connected()).first();
+		Calendar calendar = Calendar.findById(id);
 
-	Date aDate = new Date();
-	Date start = new DateTime().withTime(0, 0, 0, 0).toDate();
-	Date end = new DateTime().withTime(23, 59, 59, 999).toDate();
+		Date aDate = new Date();
+		Date start = new DateTime().withTime(0, 0, 0, 0).toDate();
+		Date end = new DateTime().withTime(23, 59, 59, 999).toDate();
 
-	Query query = JPA
-		.em()
-		.createQuery(
-			"from Event where lowerBound <= :start and upperBound >= :end and calendar_id=:cid order by start")
-		.setParameter("start", start, TemporalType.DATE)
-		.setParameter("end", end, TemporalType.DATE)
-		.setParameter("cid", calendar.id);
+		Query query = JPA
+				.em()
+				.createQuery(
+						"from Event where lowerBound <= :start and upperBound >= :end and calendar_id=:cid order by start")
+				.setParameter("start", start, TemporalType.DATE)
+				.setParameter("end", end, TemporalType.DATE)
+				.setParameter("cid", calendar.id);
 
-	List<Event> events = query.getResultList();
+		List<Event> events = query.getResultList();
 
-	Locale aLocale = new Locale("en", "CH");
-	render(user, calendar, events, aDate, aLocale);
-    }
+		Locale aLocale = new Locale("en", "CH");
+		render(user, calendar, events, aDate, aLocale);
+	}
 
-    public static void showDate(String nickname, Long id, Date aDate) {
-	User user = User.find("byNickname", nickname).first();
-	Calendar calendar = Calendar.findById(id);
+	public static void showDate(String nickname, Long id, Date aDate) {
+		User user = User.find("byNickname", nickname).first();
+		Calendar calendar = Calendar.findById(id);
 
-	Date start = new DateTime(aDate).withTime(0, 0, 0, 0).toDate();
-	Date end = new DateTime(aDate).withTime(23, 59, 59, 999).toDate();
+		Date start = new DateTime(aDate).withTime(0, 0, 0, 0).toDate();
+		Date end = new DateTime(aDate).withTime(23, 59, 59, 999).toDate();
 
-	Query query = JPA
-		.em()
-		.createQuery(
-			"from Event where lowerBound <= :start and upperBound >= :end and calendar_id=:cid order by start")
-		.setParameter("start", start, TemporalType.DATE)
-		.setParameter("end", end, TemporalType.DATE)
-		.setParameter("cid", calendar.id);
+		Query query = JPA
+				.em()
+				.createQuery(
+						"from Event where lowerBound <= :start and upperBound >= :end and calendar_id=:cid order by start")
+				.setParameter("start", start, TemporalType.DATE)
+				.setParameter("end", end, TemporalType.DATE)
+				.setParameter("cid", calendar.id);
 
-	List<Event> events = query.getResultList();
+		List<Event> events = query.getResultList();
 
-	Locale aLocale = new Locale("en", "CH");
-	render("Calendars/showCalendar.html", user, calendar, events, aDate,
-		aLocale);
-    }
+		Locale aLocale = new Locale("en", "CH");
+		render("Calendars/showCalendar.html", user, calendar, events, aDate,
+				aLocale);
+	}
 }
