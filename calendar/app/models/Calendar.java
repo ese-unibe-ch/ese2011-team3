7b2e727730c603f88cf1d0950037109329873a09
Alexander <a.rueedlinger@gmail.com>
2011-10-25 18:26:21 +0200

    -add getter User#getDefaultCalendar() to user -adapt calendar test -change Calendar#getFollowedEvents() 	modified:   calendar/app/models/Calendar.java 	modified:   calendar/app/models/User.java 	modified:   calendar/test/modelTest/CalendarTest.java

--- calendar/app/models/Calendar.java
+++ calendar/app/models/Calendar.java
@@ -18,56 +18,56 @@ import play.db.jpa.Model;
 @Entity
 public class Calendar extends Model {
 
-    public String name;
+	public String name;
 
-    @ManyToOne
-    public User owner;
+	@ManyToOne
+	public User owner;
 
-    @ManyToMany(mappedBy = "calendars")
-    public List<Event> events;
+	@ManyToMany(mappedBy = "calendars")
+	public List<Event> events;
 
-    public Calendar(String name, User owner) {
-	this.name = name;
-	this.owner = owner;
-	this.events = new ArrayList<Event>();
-    }
+	public Calendar(String name, User owner) {
+		this.name = name;
+		this.owner = owner;
+		this.events = new ArrayList<Event>();
+	}
 
-    public List<Event> getAllEventsOnDay(User user, Date aDate) {
-	Date start = new DateTime(aDate).withTime(0, 0, 0, 0).toDate();
-	Date end = new DateTime(aDate).withTime(23, 59, 59, 999).toDate();
-	Query query = JPA
-		.em()
-		.createQuery(
-			"from Event where lowerBound <= :start and upperBound >= :end  order by start")
-		.setParameter("start", start, TemporalType.DATE)
-		.setParameter("end", end, TemporalType.DATE);
+	public List<Event> getAllEventsOnDay(User user, Date aDate) {
+		Date start = new DateTime(aDate).withTime(0, 0, 0, 0).toDate();
+		Date end = new DateTime(aDate).withTime(23, 59, 59, 999).toDate();
+		Query query = JPA
+				.em()
+				.createQuery(
+						"from Event where lowerBound <= :start and upperBound >= :end  order by start")
+				.setParameter("start", start, TemporalType.DATE)
+				.setParameter("end", end, TemporalType.DATE);
 
-	List<Event> events = query.getResultList();
-	List<Event> copy = new ArrayList<Event>();
+		List<Event> events = query.getResultList();
+		List<Event> copy = new ArrayList<Event>();
 
-	for (Event ev : events) {
-	    if (ev.calendars.contains(this)) {
-		if (this.owner.equals(user)) {
-		    copy.add(ev);
-		} else {
-		    if (ev.isPublic) {
-			copy.add(ev);
-		    }
+		for (Event ev : events) {
+			if (ev.calendars.contains(this)) {
+				if (this.owner.equals(user)) {
+					copy.add(ev);
+				} else {
+					if (ev.isPublic) {
+						copy.add(ev);
+					}
+				}
+			}
 		}
-	    }
+
+		return copy;
 	}
 
-	return copy;
-    }
-
-    public List<Event> getFollowedEvents() {
-	List<Event> followedEvents = new ArrayList<Event>();
-	for (Event ev : this.events) {
-	    if (!ev.owner.equals(this.owner)) {
-		followedEvents.add(ev);
-	    }
+	public List<Event> getFollowedEvents() {
+		List<Event> followedEvents = new ArrayList<Event>();
+		for (Event ev : this.events) {
+			if (!ev.owner.equals(this.owner)) {
+				followedEvents.add(ev);
+			}
+		}
+		return followedEvents;
 	}
-	return followedEvents;
-    }
 
 }
--- calendar/app/models/User.java
+++ calendar/app/models/User.java
@@ -9,66 +9,80 @@ import javax.persistence.Lob;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
-import controllers.Calendars;
-
 import play.db.jpa.Model;
 
 @Entity
 public class User extends Model implements Comparable<User> {
 
-    public String nickname;
-    public String fullname;
-    public String password;
-    public String mail;
+	public String nickname;
+	public String fullname;
+	public String password;
+	public String mail;
 
-    @Lob
-    public String profile;
+	@Lob
+	public String profile;
 
-    @OneToMany(mappedBy = "owner")
-    public List<Event> events;
+	@OneToMany(mappedBy = "owner")
+	public List<Event> events;
 
-    @ManyToMany
-    public List<User> following;
+	@ManyToMany
+	public List<User> following;
 
-    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL)
-    public List<Calendar> calendars;
+	@OneToMany(mappedBy = "owner", cascade = CascadeType.ALL)
+	public List<Calendar> calendars;
 
-    public Calendar defaultCalendar = null;
+	public Calendar defaultCalendar = null;
 
-    public User(String nick, String name, String pass, String mail) {
-	this.nickname = nick;
-	this.fullname = name;
-	this.password = pass;
-	this.mail = mail;
-	this.following = new ArrayList<User>();
-	this.events = new ArrayList<Event>();
-	this.calendars = new ArrayList<Calendar>();
+	public User(String nick, String name, String pass, String mail) {
+		this.nickname = nick;
+		this.fullname = name;
+		this.password = pass;
+		this.mail = mail;
+		this.following = new ArrayList<User>();
+		this.events = new ArrayList<Event>();
+		this.calendars = new ArrayList<Calendar>();
 
-    }
+	}
 
-    public static User connect(String nickname, String password) {
-	return User.find("byNicknameAndPassword", nickname, password).first();
-    }
+	public static User connect(String nickname, String password) {
+		return User.find("byNicknameAndPassword", nickname, password).first();
+	}
 
-    public int compareTo(User u) {
-	int compare = nickname.compareTo(u.nickname);
-	return compare;
-    }
-    //should be used to add calendars
+	public Calendar getDefaultCalendar() {
+		if (this.defaultCalendar != null) {
+			if (this.calendars.isEmpty()) {
+				this.defaultCalendar = new Calendar(
+						this.fullname + " Calendar", this);
+			} else {
+				this.defaultCalendar = this.calendars.get(0);
+			}
+		}
+		return this.defaultCalendar;
+	}
+
+	public int compareTo(User u) {
+		int compare = nickname.compareTo(u.nickname);
+		return compare;
+	}
+
+	// should be used to add calendars
 	public void addCalendar(Calendar calendar) {
-		if(this.defaultCalendar == null){
+		if (this.defaultCalendar == null) {
 			this.defaultCalendar = calendar;
 		}
 		this.calendars.add(calendar);
 	}
-	//TODO: set defaultCalendar correctly. why is the list calendars empty, that the JPA hands over?
-	public void setCalendars(List<Calendar> calendars){
+
+	// TODO: set defaultCalendar correctly. why is the list calendars empty,
+	// that the JPA hands over?
+	public void setCalendars(List<Calendar> calendars) {
 		this.calendars = calendars;
-		if(this.defaultCalendar == null && !calendars.isEmpty()){
+		if (this.defaultCalendar == null && !calendars.isEmpty()) {
 			this.defaultCalendar = calendars.get(0);
 		}
 	}
-	public List<Calendar> getCalendars(){
+
+	public List<Calendar> getCalendars() {
 		return this.calendars;
 	}
 }
