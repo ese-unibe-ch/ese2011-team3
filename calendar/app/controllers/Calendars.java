8a27fce1134852fc602246912e08fb2d1b425403
Julian Pollak <julian.pollak@students.unibe.ch>
2011-10-26 16:48:25 +0200

    Prepare to write the saveEvent method.

--- calendar/app/controllers/Calendars.java
+++ calendar/app/controllers/Calendars.java
@@ -12,14 +12,10 @@ import models.User;
 import org.joda.time.DateTime;
 
 import play.db.jpa.JPA;
-import play.mvc.Before;
 import play.mvc.With;
 
 @With(Secure.class)
 public class Calendars extends Main {
-	@Before(unless = {
-		"index"
-	})
 	private static void putCalendarData(Long calendarId, Date currentDate) {
 		Query calendarQuery = JPA.em()
 				.createQuery("SELECT c FROM Calendar c WHERE c.id = :id")
@@ -45,15 +41,18 @@ public class Calendars extends Main {
 	}
 
 	public static void viewCalendar(Long calendarId, Date currentDate) {
+		putCalendarData(calendarId, currentDate);
 		render();
 	}
 
 	public static void viewNextMonth(Long calendarId, Date currentDate) {
+		putCalendarData(calendarId, currentDate);
 		viewCalendar(calendarId, new DateTime(currentDate).plusMonths(1)
 				.toDate());
 	}
 
 	public static void viewPrevMonth(Long calendarId, Date currentDate) {
+		putCalendarData(calendarId, currentDate);
 		viewCalendar(calendarId, new DateTime(currentDate).minusMonths(1)
 				.toDate());
 	}
@@ -61,12 +60,14 @@ public class Calendars extends Main {
 	public static void editCalendar() {}
 
 	public static void addEvent(Long calendarId, Date currentDate) {
+		putCalendarData(calendarId, currentDate);
 		renderArgs.put("actionName", "Add Event");
 
 		renderTemplate("Calendars/viewEvent.html");
 	}
 
 	public static void editEvent(Long calendarId, Date currentDate, Long eventId) {
+		putCalendarData(calendarId, currentDate);
 		renderArgs.put("actionName", "Edit Event");
 
 		Query eventQuery = JPA.em()
@@ -74,17 +75,28 @@ public class Calendars extends Main {
 				.setParameter("id", eventId);
 		Event event = (Event) eventQuery.getSingleResult();
 
-		Calendars.flash.put("name", event.name);
-		Calendars.flash.put("startDate", event.start);
-		Calendars.flash.put("endDate", event.end);
-		Calendars.flash.put("isPublic", event.isPublic);
-		Calendars.flash.put("note", event.note);
+		flash.put("id", event.id);
+		flash.put("name", event.name);
+		flash.put("startDate", new DateTime(event.start).toString("yyyy-MM-dd"));
+		flash.put("endDate", new DateTime(event.end).toString("yyyy-MM-dd"));
+		flash.put("startTime", new DateTime(event.start).toString("HH:mm"));
+		flash.put("endTime", new DateTime(event.end).toString("HH:mm"));
+		flash.put("isPublic", event.isPublic);
+		flash.put("note", event.note);
 
 		renderTemplate("Calendars/viewEvent.html");
 	}
 
+	public static void saveEvent(Long calendarId, Date currentDate) {
+		// save logic
+
+		// put start date of the event here!!
+		viewCalendar(calendarId, new Date());
+	}
+
 	public static void deleteEvent(Long calendarId, Date currentDate,
 			Long eventId) {
+		putCalendarData(calendarId, currentDate);
 		viewCalendar(calendarId, currentDate);
 	}
 
