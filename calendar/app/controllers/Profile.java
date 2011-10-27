22359491f64bcca1665cfebe5ef0f7268b1527c0
Julian Pollak <julian.pollak@students.unibe.ch>
2011-10-27 13:45:18 +0200

    DONE!! DONE DONE DONE DONE!!!!!
    ha ha ha ha ha this is sooooo funny (run it over espeak xD).

--- calendar/app/controllers/Profile.java
+++ calendar/app/controllers/Profile.java
@@ -1,11 +1,15 @@
 package controllers;
 
+import models.User;
+
 public class Profile extends Main {
 	public static void index() {
-
+		viewProfile(getUser().id);
 	}
 
 	public static void viewProfile(Long userId) {
+		renderArgs.put("user", User.findById(userId));
 
+		render();
 	}
 }
