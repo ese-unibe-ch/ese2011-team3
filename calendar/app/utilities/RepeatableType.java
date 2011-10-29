1989709edefe8e03efa25345fba29d3f3a190fed
Alexander <a.rueedlinger@gmail.com>
2011-10-29 20:01:44 +0200

    add repetable type for events 	modified:   calendar/app/models/Event.java 	new file:   calendar/app/utilities/RepeatableType.java

---
+++ calendar/app/utilities/RepeatableType.java
@@ -0,0 +1,46 @@
+package utilities;
+
+import java.util.Date;
+
+import models.Event;
+
+public enum RepeatableType {
+
+    DAILY {
+
+	public boolean happensOnDay(Event event, Date aDay) {
+	    // TODO
+	    return false;
+	}
+    },
+    WEEKLY {
+	public boolean happensOnDay(Event event, Date aDay) {
+	    // TODO
+	    return false;
+	}
+    },
+    MONTHLY {
+
+	public boolean happensOnDay(Event event, Date aDay) {
+	    // TODO
+	    return false;
+	}
+    },
+    YEARLY {
+
+	public boolean happensOnDay(Event event, Date aDay) {
+	    // TODO
+	    return false;
+	}
+    },
+
+    NO_REPETITION {
+
+	public boolean happensOnDay(Event event, Date aDay) {
+	    return false;
+	}
+    };
+
+    public abstract boolean happensOnDay(Event event, Date aDay);
+
+}
