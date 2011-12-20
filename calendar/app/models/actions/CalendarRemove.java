fa3d3eba4d8c1cea20ef1dc67c799c839e31b439
Alexander <a.rueedlinger@gmail.com>
2011-12-20 13:49:44 +0100

    add calendar remove action

---
+++ calendar/app/models/actions/CalendarRemove.java
@@ -0,0 +1,17 @@
+package models.actions;
+
+public class CalendarRemove extends AbstractAction {
+
+    @Override
+    public void execute() {
+	// TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void undo() {
+	// TODO Auto-generated method stub
+
+    }
+
+}
